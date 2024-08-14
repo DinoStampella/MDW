39d56258ddadb2c8873c84597ca4a7ab949ea5b8
Dino Stampella <dinostampella@Dinos-MacBook-Pro.local>
2024-08-13 21:42:17 -0300

    Initial commit

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,19 @@
+export const createUser = (req, res) => {
+  res.json({ message: "", data: {} });
+};
+
+export const getUsers = (req, res) => {
+  res.json({ message: "", data: {} });
+};
+
+export const getUser = (req, res) => {
+  res.json({ message: "", data: {} });
+};
+
+export const updateUser = (req, res) => {
+  res.json({ message: "", data: {} });
+};
+
+export const deleteUser = (req, res) => {
+  res.json({ message: "", data: {} });
+};
---
+++ src/routes/users.route.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+  createUser,
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/index.js";
+
+const router = Router();
+
+router.get("/", getUsers);
+
+router.get("/:id", getUser);
+
+router.post("/", createUser);
+
+router.put("/:id", updateUser);
+
+router.delete("/:id", deleteUser);
+
+export default router;
