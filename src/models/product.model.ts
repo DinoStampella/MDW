cd26e0776d5b199ee18bc7e920315a3a78d8a161
Dino <dino.stampella96@gmail.com>
2024-08-27 21:02:54 -0300

    Define schemas

---
+++ src/models/product.model.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface Product extends Document {
+  name: string;
+  description: string;
+  amount: number;
+  price: number;
+  isActive: boolean;
+  image: string;
+  ownerId: string;
+}
+
+const ProductSchema: Schema = new Schema(
+  {
+    name: { type: String },
+    description: { type: String },
+    amout: { type: Number },
+    price: { type: Number },
+    isActive: { type: Boolean },
+    image: { type: String },
+    ownerId: {type: mongoose.Schema.Types.ObjectId, ref: "User"}
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Product = mongoose.model<Product>("Product", ProductSchema);
